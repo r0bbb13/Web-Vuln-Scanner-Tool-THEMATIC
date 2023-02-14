9b9be1135950272dfaaaf8c37c1a05d3a28905f5
Robyn <95867399+r0bbb13@users.noreply.github.com>
2023-02-14 13:55:07 +0000

    Update main.py
    added subproccess

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from BeautifulSoup import BeautifulSoup
 import pandas as pd
+#this allows script to run commands into terminal 
+from subprocess import call 
 
-//may need this later
-//driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+#may need this later
+#driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
 URL = input("Please enter the sites URL")
 
-
+print("checking {URL} for vulnerabilities")
