0c02320a7afc12546661b4889213128ee25031c4
Robyn <95867399+r0bbb13@users.noreply.github.com>
2023-02-14 13:31:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from selenium import webdriver
 from BeautifulSoup import BeautifulSoup
 import pandas as pd
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+//may need this later
+//driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+
+URL = input("Please enter the sites URL")
+
+
